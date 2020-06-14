9cc140709ed182a3c04684ab9880d0d6d7bf409a
Junior <fernando.j.junior@accenture.com>
2020-06-14 04:55:21 -0300

    add maps

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://192.168.0.18:3001",
-  // baseURL: "https://fastfeetbackend-fj.herokuapp.com",
 });
 
 export default api;
